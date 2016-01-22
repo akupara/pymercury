a0b5849fedbcdd19bab8939838127e6b2e86d081
Lei Sun <leix.sun@qq.com>
2016-01-22 14:58:39 +0800

    增加create_session()方法，通过alembic的op构建sqlalchemy的session对象

--- pymercury/alembicutils.py
+++ pymercury/alembicutils.py
@@ -3,6 +3,46 @@
 import os
 
 
+def create_session():
+    """
+    Create a SQLAchemy session object using the connection of alembic
+    :return:
+    """
+    from alembic import op
+    from sqlalchemy.orm import sessionmaker
+    Session = sessionmaker(bind=op.get_bind())
+    return Session()
+
+
+def reflect_model(tablename):
+    """
+    Create a SQLAlchemy model dynamically based on current table schema in database
+    :param tablename: table name of the model you want to reflect
+    :return:
+    """
+    from alembic import op
+    from sqlalchemy.ext.automap import automap_base
+    Base = automap_base()
+    Base.prepare(op.get_bind().engine, reflect=True)
+    return Base.classes[tablename]
+
+
+def bulk_insert_data(tablename, rows, multiinsert=True):
+    """
+    Construct a model by inspecting the existing table schema. Then insert the rows against it.
+    :param tablename: The table name of the rows will be inserted.
+    :param rows: a list of dictionaries indicating rows
+    :return:
+    """
+    if not isinstance(rows, list):
+        raise TypeError('rows parameter is expected to be list type')
+    elif rows and not isinstance(rows[0], dict):
+        raise TypeError("rows parameter is expected to be list of dict type")
+
+    from alembic import op
+    op.bulk_insert(reflect_model(tablename).__table__, rows, multiinsert)
+
+
 def absolute_path(relative_path):
     """
     :param relative_path: the path relative to alembic migration folder
@@ -25,27 +65,3 @@ def parse_csv_with_header(csv_file_path):
     with open(csv_file_path, 'r') as csv_file:
         from unicodecsv import DictReader
         return [row for row in DictReader(csv_file)]
-
-
-def reflect_model(tablename):
-    from alembic import op
-    from sqlalchemy.ext.automap import automap_base
-    Base = automap_base()
-    Base.prepare(op.get_bind().engine, reflect=True)
-    return Base.classes[tablename]
-
-
-def bulk_insert_data(tablename, rows, multiinsert=True):
-    """
-    Construct a model by inspecting the existing table schema. Then insert the rows against it.
-    :param tablename: The table name of the rows will be inserted.
-    :param rows: a list of dictionaries indicating rows
-    :return:
-    """
-    if not isinstance(rows, list):
-        raise TypeError('rows parameter is expected to be list type')
-    elif rows and not isinstance(rows[0], dict):
-        raise TypeError("rows parameter is expected to be list of dict type")
-
-    from alembic import op
-    op.bulk_insert(reflect_model(tablename).__table__, rows, multiinsert)
